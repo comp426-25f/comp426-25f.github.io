2dfeaab0844bbf51eb0b0985f9932ec448e52f10
Ajay Gandecha <ajgandecha@gmail.com>
2025-07-22 22:44:56 -0400

    chore: get minimal working version of content
    Gets the minimal working version of content rendering correctly (via the docs directory)

---
+++ app/(docs)/layout.tsx
@@ -0,0 +1,15 @@
+import { SiteFooter } from "@/components/site-footer"
+import Link from "next/link"
+
+interface DocsLayoutProps {
+  children: React.ReactNode
+}
+
+export default function DocsLayout({ children }: DocsLayoutProps) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <div className="container flex-1">{children}</div>
+      <SiteFooter className="border-t" />
+    </div>
+  )
+}
---
+++ config/site.ts
@@ -0,0 +1,8 @@
+import { SiteConfig } from "@/types"
+
+export const siteConfig: SiteConfig = {
+  name: "COMP 426 (F'25)",
+  description:
+    "Course website for the Fall 2025 offering of COMP 426: Modern Web Programming @ UNC-Chapel Hill.",
+  url: "https://comp426-25f.github.io",
+}
