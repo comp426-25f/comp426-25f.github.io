38e73070fbdbbd0497c5d666aeb84d5888df2f5b
Ajay Gandecha <ajgandecha@gmail.com>
2025-07-24 21:55:59 -0400

    chore: add badge to assignment pages
    Adds the assignment badge to the assignment pages.

--- app/(content)/supplements/[[...slug]]/page.tsx
+++ app/(content)/supplements/[[...slug]]/page.tsx
@@ -3,6 +3,8 @@ import '@/styles/mdx.css';
 import { generateNextjsContentPage } from '@/lib/content';
 import { ContentHeader } from '@/components/content-header';
 import ContentLayout from '@/components/layouts/content-layout';
+import { ColoredBadge } from '@/components/colored-badge';
+import { formatShortDateFromString } from '@/lib/date';
 
 const {
   generateMetadata,
@@ -14,9 +16,17 @@ const {
       <ContentHeader
         title={doc.title}
         headlineElement={
-          <p className="mb-2 text-sm text-muted-foreground">
-            Due on Mon Apr 9, 2025
-          </p>
+          <div className="flex flex-row items-center mb-2 gap-3">
+            <ColoredBadge text={doc.code} color="purple" />
+            {doc.publishedDate && (
+              <p className="text-sm text-muted-foreground">
+                Published on {formatShortDateFromString(doc.publishedDate)}
+              </p>
+            )}
+          </div>
+          // <p className="mb-2 text-sm text-muted-foreground">
+          //   Due on Mon Apr 9, 2025
+          // </p>
         }
         authors={doc.authors}
       />
--- components/home/assignment-list-item.tsx
+++ components/home/assignment-list-item.tsx
@@ -1,36 +1,17 @@
 'use client';
 
 import { Assignment, Reading } from '@/.contentlayer/generated';
-import { DueStatus, dueStatus, formatDateFromString } from '@/lib/date';
+import { DueStatus, dueStatus } from '@/lib/date';
 import { useRouter } from 'next/navigation';
 import { AssignmentBadge } from './assignment-badge';
 import { cn } from '@/lib/utils';
+import { DueText } from './due-text';
 
 export type AssignmentListItemType = Reading | Assignment;
 
 export function AssignmentListItem({ item }: { item: AssignmentListItemType }) {
   const router = useRouter();
   const itemDueStatus: DueStatus | null = item.due ? dueStatus(item.due) : null;
-
-  const dueElement = (due: string) => {
-    return itemDueStatus === 'past' ? (
-      <p className="md:ml-auto text-sm text-rose-500">
-        Due: {formatDateFromString(due)}
-      </p>
-    ) : itemDueStatus === 'tonight' ? (
-      <p className="md:ml-auto text-sm text-rose-500">
-        Due tonight: {formatDateFromString(due)}
-      </p>
-    ) : itemDueStatus === 'soon' ? (
-      <p className="md:ml-auto text-sm text-amber-600">
-        Due soon: {formatDateFromString(due)}
-      </p>
-    ) : (
-      <p className="md:ml-auto text-sm text-muted-foreground">
-        Due: {formatDateFromString(due)}
-      </p>
-    );
-  };
 
   return (
     <div
@@ -43,7 +24,13 @@ export function AssignmentListItem({ item }: { item: AssignmentListItemType }) {
       <AssignmentBadge code={item.code} />
       <div className="flex flex-col md:flex-row md:items-center md:justify-between w-full">
         <p className="font-semibold pb-0.5">{item.title}</p>
-        {item.due && dueElement(item.due)}
+        {item.due && (
+          <DueText
+            itemDueStatus={itemDueStatus}
+            due={item.due}
+            className="md:ml-auto"
+          />
+        )}
       </div>
     </div>
   );
