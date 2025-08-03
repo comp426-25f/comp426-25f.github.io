a990113e333c5d34f4bd89271fae6a2cab055dc6
Ajay Gandecha <ajgandecha@gmail.com>
2025-08-03 14:38:21 -0400

    Revert "chore: setup"

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
-import type { NextConfig } from 'next';
-import { withContentlayer } from 'next-contentlayer2';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true
-  }
 };
 
-export default withContentlayer(nextConfig);
+export default nextConfig;
