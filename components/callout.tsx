de4c3c59d11ba7e2cbfa186e1bf480d2b977cae8
Ajay Gandecha <ajgandecha@gmail.com>
2025-08-17 13:40:56 -0400

    chore: update callout colors in dark mode

--- components/callout.tsx
+++ components/callout.tsx
@@ -15,11 +15,15 @@ export function Callout({
   return (
     <div
       className={cn('my-6 flex items-start rounded-md border border-l-4 p-4', {
-        'border-red-900 bg-red-50': type === 'red',
-        'border-yellow-900 bg-yellow-50': type === 'yellow',
+        'border-red-900 bg-red-50 dark:border-red-50/40 dark:bg-red-600/40':
+          type === 'red',
+        'border-yellow-900 bg-yellow-50 dark:border-yellow-50/40 dark:bg-yellow-600/40':
+          type === 'yellow',
         'border-blue-900 bg-blue-50': type === 'blue',
-        'border-green-900 bg-green-50': type === 'green',
-        'border-slate-600 bg-slate-100': type === 'default'
+        'border-green-900 bg-green-50 dark:border-green-50/40 dark:bg-green-700/40':
+          type === 'green',
+        'border-slate-600 bg-slate-100 dark:border-slate-50/40 dark:bg-slate-600/40':
+          type === 'default'
       })}
       {...props}>
       {icon && <span className="mr-4 text-2xl">{icon}</span>}
