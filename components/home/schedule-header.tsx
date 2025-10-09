6cae0f7968278f22ad1f09923f199d33e9c2072b
Ajay Gandecha <ajgandecha@gmail.com>
2025-10-09 15:22:33 -0400

    chore: add l14

--- components/home/schedule-header.tsx
+++ components/home/schedule-header.tsx
@@ -16,6 +16,7 @@ export function ScheduleHeader() {
         {legendItem('lightblue', 'Fundamentals')}
         {legendItem('blue', 'React')}
         {legendItem('purple', 'Next.js')}
+        {legendItem('green', 'Backend')}
         {/* {legendItem('blue', 'React')}
         {legendItem('purple', 'Next.js')}
         {legendItem('green', 'Backend')}
